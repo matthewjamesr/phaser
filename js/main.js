5294f2e27265f1c5bdc737019ab35111cfc9bd99
Matthew Reichardt <me@matthewreichardt.com>
2021-04-22 11:44:06 -0500

    Test for 3d buildings

--- js/main.js
+++ js/main.js
@@ -172,6 +172,43 @@ var app = new Vue({
                         'sky-atmosphere-sun-intensity': 15
                     }
                 })
+                map.addLayer({
+                    'id': 'add-3d-buildings',
+                    'source': 'composite',
+                    'source-layer': 'building',
+                    'filter': ['==', 'extrude', 'true'],
+                    'type': 'fill-extrusion',
+                    'minzoom': 15,
+                    'paint': {
+                        'fill-extrusion-color': '#aaa',
+
+                        // Use an 'interpolate' expression to
+                        // add a smooth transition effect to
+                        // the buildings as the user zooms in.
+                        'fill-extrusion-height': [
+                            'interpolate',
+                            ['linear'],
+                            ['zoom'],
+                            15,
+                            0,
+                            15.05,
+                            ['get', 'height']
+                        ],
+                        'fill-extrusion-base': [
+                            'interpolate',
+                            ['linear'],
+                            ['zoom'],
+                            15,
+                            0,
+                            15.05,
+                            ['get', 'min_height']
+                        ],
+                        'fill-extrusion-opacity': 0.6
+                        }
+                    },
+
+                    labelLayerId
+                )
                 self.map.addSource('informationPoints', {
                     'type': 'geojson',
                     'data': self.informationCollection
