4f8de46085553e669a7b6bae40a0175064911b8d
Matthew Reichardt <me@matthewreichardt.com>
2021-04-22 12:07:22 -0500

    Revert 3d tests

--- js/main.js
+++ js/main.js
@@ -172,52 +172,6 @@ var app = new Vue({
                         'sky-atmosphere-sun-intensity': 15
                     }
                 })
-                // Insert the layer beneath any symbol layer.
-                var layers = map.getStyle().layers;
-                var labelLayerId;
-                for (var i = 0; i < layers.length; i++) {
-                    if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-                        labelLayerId = layers[i].id;
-                        break;
-                    }
-                }
-                self.map.addLayer({
-                    'id': 'add-3d-buildings',
-                    'source': 'composite',
-                    'source-layer': 'building',
-                    'filter': ['==', 'extrude', 'true'],
-                    'type': 'fill-extrusion',
-                    'minzoom': 15,
-                    'paint': {
-                        'fill-extrusion-color': '#aaa',
-
-                        // Use an 'interpolate' expression to
-                        // add a smooth transition effect to
-                        // the buildings as the user zooms in.
-                        'fill-extrusion-height': [
-                            'interpolate',
-                            ['linear'],
-                            ['zoom'],
-                            15,
-                            0,
-                            15.05,
-                            ['get', 'height']
-                        ],
-                        'fill-extrusion-base': [
-                            'interpolate',
-                            ['linear'],
-                            ['zoom'],
-                            15,
-                            0,
-                            15.05,
-                            ['get', 'min_height']
-                        ],
-                        'fill-extrusion-opacity': 0.6
-                        }
-                    },
-
-                    labelLayerId
-                )
                 self.map.addSource('informationPoints', {
                     'type': 'geojson',
                     'data': self.informationCollection
